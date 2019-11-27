b0a3a10ef9e0acba77fe9b88d1a34efff3bf2ad5
Ian Abraham <iabr4073@gmail.com>
2019-11-27 15:12:56 -0600

    ddpg + model based control gives weird noisey large controls

---
+++ ddpg/__init__.py
@@ -0,0 +1,4 @@
+from .ddpg import DDPG
+from .ou_noise import OUNoise
+from .policynetwork import PolicyNetwork
+from .replay_buffer import ReplayBuffer
--- ddpg/ddpg.py
+++ ddpg/ddpg.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-from valuenetwork import ValueNetwork
+from .valuenetwork import ValueNetwork
 
 
 
