6b8cde6c8e4cc88ccbb60447b0b329314d18bd85
Ian Abraham <iabr4073@gmail.com>
2019-12-16 17:10:00 -0600

    lunar lander has issues with landing reward

--- benchmarks/batch_examples/sac_lunar_lander.sh
+++ benchmarks/batch_examples/sac_lunar_lander.sh
@@ -2,6 +2,6 @@
 trials=2
 for i in {1..$trials}
 do
-    python3 sac_bm.py --env 'LunarLanderContinuousEnv' --max_steps 400 --policy_lr 3e-4 --max_frames 6000 --frame_skip 1 --no_render
+    python3 sac_bm.py --env 'LunarLanderContinuousEnv' --max_steps 100 --policy_lr 3e-4 --max_frames 10000 --frame_skip 2 --render
     echo "trial $i out of $trials"
 done
