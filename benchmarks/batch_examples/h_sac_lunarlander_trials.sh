94614a52190986c5a17a6c3268755d2ce8450555
Ian Abraham <iabr4073@gmail.com>
2019-12-16 15:51:01 -0600

    added some fixes to some random stuff

--- benchmarks/batch_examples/h_sac_lunarlander_trials.sh
+++ benchmarks/batch_examples/h_sac_lunarlander_trials.sh
@@ -2,6 +2,6 @@
 trials=2
 for i in {1..$trials}
 do
-    python3 h_sac.py --env 'LunarLanderContinuousEnv' --max_steps 600 --max_frames 6000 --frame_skip 1 --render --model_lr 3e-3 --policy_lr 3e-4
+    python3 h_sac.py --env 'LunarLanderContinuousEnv' --max_steps 100 --max_frames 6000 --frame_skip 2 --horizon 10 --render --model_lr 3e-3 --policy_lr 3e-4
     echo "trial $i out of $trials"
 done
