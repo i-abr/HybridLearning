882d601643dc3f0985b981a9b255f815342ed6ba
apinosky <alpinosky@gmail.com>
2020-01-22 17:56:58 -0600

    added angle to ik_move

--- benchmarks/h_sac.py
+++ benchmarks/h_sac.py
@@ -7,7 +7,7 @@ import os
 sys.path.append('../')
 
 # local imports
-import envs
+# import envs
 
 import torch
 from sac import SoftActorCritic
@@ -21,7 +21,7 @@ from model import ModelOptimizer, Model, SARSAReplayBuffer
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--env',        type=str,   help=envs.getlist())
+# parser.add_argument('--env',        type=str,   help=envs.getlist())
 parser.add_argument('--max_steps',  type=int,   default=200)
 parser.add_argument('--max_frames', type=int,   default=10000)
 parser.add_argument('--frame_skip', type=int,   default=2)
@@ -40,60 +40,25 @@ parser.add_argument('--done_util', dest='done_util', action='store_true')
 parser.add_argument('--no_done_util', dest='done_util', action='store_false')
 parser.set_defaults(done_util=True)
 
-parser.add_argument('--render', dest='render', action='store_true')
-parser.add_argument('--no_render', dest='render', action='store_false')
-parser.set_defaults(render=False)
+# parser.add_argument('--render', dest='render', action='store_true')
+# parser.add_argument('--no_render', dest='render', action='store_false')
+# parser.set_defaults(render=False)
 
 args = parser.parse_args()
 
 
-def get_expert_data(env, replay_buffer, T=200):
-    state = env.reset()
-    for t in range(T):
-        action = expert(env, state)
-        next_state, reward, done, info = env.step(action)
-        replay_buffer.push(state, action, reward, next_state, done)
+class sawer_env(object):
+    def init:
+    # include lookup table
+    def reset(self):
 
-        state = next_state
-
-        if done:
-            break
-
-def test_with_planner(env, planner, max_steps=200):
-        state = env.reset()
-        planner.reset()
-        episode_reward = 0
-        for step in range(max_steps):
-            action = planner(state)
-            next_state, reward, done, _ = env.step(action)
-            state = next_state
-            episode_reward += reward
-            if done:
-                break
-        return episode_reward
+    def step(self, action):
+        # publishes action input
+        # returns reward state and if it's outside bounds
+    def cost_function(self):
 
 if __name__ == '__main__':
-
-    # env_name = 'KukaBulletEnv-v0'
-    # env_name = 'InvertedPendulumSwingupBulletEnv-v0'
-    # env_name = 'ReacherBulletEnv-v0'
-    # env_name = 'HalfCheetahBulletEnv-v0'
-    # env = pybullet_envs.make(env_name)
-    # env.isRender = True
-    # env = KukaGymEnv(renders=True, isDiscrete=False)
-
-
-    env_name = args.env
-    try:
-        env = envs.env_list[env_name](render=args.render)
-    except TypeError as err:
-        print('no argument render,  assumping env.render will just work')
-        env = envs.env_list[env_name]()
-    env.reset()
-    print(env.action_space.low, env.action_space.high)
-    assert np.any(np.abs(env.action_space.low) <= 1.) and  np.any(np.abs(env.action_space.high) <= 1.), 'Action space not normalizd'
-
-
+    env_name = 'sawyer'
     now = datetime.now()
     date_str = now.strftime("%Y-%m-%d_%H-%M-%S/")
 
@@ -101,14 +66,13 @@ if __name__ == '__main__':
     if os.path.exists(path) is False:
         os.makedirs(path)
 
-    action_dim = env.action_space.shape[0]
-    state_dim  = env.observation_space.shape[0]
+    action_dim = 2 # env.action_space.shape[0]
+    state_dim  = 4 # env.observation_space.shape[0]
     hidden_dim = 128
 
     policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim)
 
     model = Model(state_dim, action_dim, def_layers=[200])
-    # model = MDNModel(state_dim, action_dim, def_layers=[200, 200])
 
 
     replay_buffer_size = 1000000
@@ -116,9 +80,6 @@ if __name__ == '__main__':
 
     model_replay_buffer = SARSAReplayBuffer(replay_buffer_size)
     model_optim = ModelOptimizer(model, model_replay_buffer, lr=args.model_lr)
-
-    # model_optim = MDNModelOptimizer(model, replay_buffer, lr=args.model_lr)
-
 
     sac = SoftActorCritic(policy=policy_net,
                           state_dim=state_dim,
@@ -138,7 +99,6 @@ if __name__ == '__main__':
     rewards     = []
     batch_size  = 128
 
-    # env.camera_adjust()
     ep_num = 0
     while frame_idx < max_frames:
         state = env.reset()
@@ -148,7 +108,6 @@ if __name__ == '__main__':
 
         episode_reward = 0
         for step in range(max_steps):
-            # action = policy_net.get_action(state)
             for _ in range(frame_skip):
                 next_state, reward, done, _ = env.step(action.copy())
 
@@ -167,9 +126,8 @@ if __name__ == '__main__':
             episode_reward += reward
             frame_idx += 1
 
-            if args.render:
-                env.render("human")
-
+            # if args.render:
+            #     env.render("human")
 
             if frame_idx % int(max_frames/10) == 0:
                 print(
--- sawyer/README.md
+++ sawyer/README.md
@@ -9,8 +9,10 @@ rosrun intera_interface enable_robot.py -s
 source ~/murphey_lab/sawyer_ws/devel/setup.bash
 
 # go to start position
+# higher
+rosrun intera_examples go_to_joint_angles.py -q -0.2 -0.6 0.0 1.9 0.0 0.3 1.571 -s 0.6
 # lower
-rosrun intera_examples go_to_joint_angles.py -q -0.1 -0.3 0.0 1.6 0.0 0.3 1.71 -s 0.8
+rosrun intera_examples go_to_joint_angles.py -q -0.1 -0.3 0.0 1.6 0.0 0.3 1.571 -s 0.8
 
 # run joint action trajectory server
 rosrun intera_interface joint_trajectory_action_server.py
