cad7e20be7dead09be951c7f86214188d629e71c
apinosky <alpinosky@gmail.com>
2020-01-27 16:48:20 -0600

    h_sac updates

--- benchmarks/h_sac.py
+++ benchmarks/h_sac.py
@@ -22,7 +22,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 # parser.add_argument('--env',        type=str,   help=envs.getlist())
-parser.add_argument('--max_steps',  type=int,   default=200)
+parser.add_argument('--max_steps',  type=int,   default=1000)
 parser.add_argument('--max_frames', type=int,   default=10000)
 parser.add_argument('--frame_skip', type=int,   default=2)
 parser.add_argument('--model_lr',   type=float, default=3e-3)
@@ -54,12 +54,20 @@ from std_srvs.srv import Empty, EmptyResponse
 import tf
 import time
 
-class april_tags(object):
+class sawer_env(object):
     def __init__(self):
+        self.move = rospy.Publisher('/puck/relative_move',RelativeMove,queue_size=1)
+        self.reset_arm = rospy.ServiceProxy('/puck/reset', Empty)
+        rospy.wait_for_service('/puck/reset', 5.0)
         self.listener = tf.TransformListener()
 
-    def get_transform_between_frames(self, reference_frame, target_frame):
-        time_out = 0.3
+        # set up tf
+        target_transform = self.setup_transform_between_frames( 'target','block2')
+        ee_transform = self.setup_transform_between_frames('target','ee')
+        self.state = np.array([ee_transform[0],ee_transform[1],target_transform[0],target_transform[1]])
+
+    def setup_transform_between_frames(self, reference_frame, target_frame):
+        time_out = 0.5
         start_time = time.time()
         while(True):
             try:
@@ -71,26 +79,19 @@ class april_tags(object):
         return translation
 
     def get_transforms(self):
-        target_transform = self.get_transform_between_frames( 'target','block2')
-        ee_transform = self.get_transform_between_frames('target','ee')
-        rospy.loginfo("Target tf: %s", target_transform)
-        rospy.loginfo("EE tf: %s", ee_transform)
+        try:
+            target_transform, _ = self.listener.lookupTransform( 'target','block2', rospy.Time(0))
+            ee_transform, _ = self.listener.lookupTransform( 'target','ee', rospy.Time(0))
 
-        state = np.array([ee_transform[0],ee_transform[1],target_transform[0],target_transform[1]])
-        # state = np.array([dx_targetToArm, dy_targetToArm, dx_targetToBlock, dy_targetToBlock])
-        return state
-
-class sawer_env(object):
-    def __init__(self):
-        self.aprilTags = april_tags()
-        self.move = rospy.Publisher('/puck/relative_move',RelativeMove,queue_size=1)
-        self.reset_arm = rospy.ServiceProxy('/puck/reset', Empty)
-        rospy.wait_for_service('/puck/reset', 5.0)
+            self.state = np.array([ee_transform[0],ee_transform[1],target_transform[0],target_transform[1]])
+            # state = np.array([dx_targetToArm, dy_targetToArm, dx_targetToBlock, dy_targetToBlock])
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            pass
 
     def reset(self):
         resp = self.reset_arm()
-        state = self.aprilTags.get_transforms()
-        return state
+        self.get_transforms()
+        return self.state
 
     def step(self, _a):
         action = 0.2*np.clip(_a, -1,1)
@@ -100,17 +101,17 @@ class sawer_env(object):
         pose.dy = action[1]
         self.move.publish(pose)
         # gets the new state
-        state = self.aprilTags.get_transforms()
-        reward = self.reward_function(state)
+        self.get_transforms()
+        reward = self.reward_function()
         if (reward > -0.25):
             done = 1
         else:
             done = 0
-        return state, reward, done
+        return self.state, reward, done
 
         # returns reward state and if it's outside bounds
-    def reward_function(self,state):
-        [dx_targetToArm, dy_targetToArm, dx_targetToBlock, dy_targetToBlock] = state
+    def reward_function(self):
+        [dx_targetToArm, dy_targetToArm, dx_targetToBlock, dy_targetToBlock] = self.state
 
         arm_to_block = -((dx_targetToArm-dx_targetToBlock)**2+
                         (dy_targetToArm-dy_targetToBlock)**2)*100
@@ -124,106 +125,119 @@ class sawer_env(object):
 
         return reward
 
+class h_sac(object):
+    def doneCallback(self,req):
+        self.done = 1
+        print('manual done called')
+        return EmptyResponse()
+
+    def main(self):
+        env = sawer_env()
+        manual_done = rospy.Service('/puck/done', Empty, self.doneCallback)
+
+        env_name = 'sawyer'
+        now = datetime.now()
+        date_str = now.strftime("%Y-%m-%d_%H-%M-%S/")
+
+        path = './data/' + env_name +  '/' + 'h_sac/' + date_str
+        if os.path.exists(path) is False:
+            os.makedirs(path)
+
+
+        action_dim = 2 # env.action_space.shape[0]
+        state_dim  = 4 # env.observation_space.shape[0]
+        hidden_dim = 128
+
+        policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim)
+
+        model = Model(state_dim, action_dim, def_layers=[200])
+
+        replay_buffer_size = 1000000
+        replay_buffer = ReplayBuffer(replay_buffer_size)
+
+        model_replay_buffer = SARSAReplayBuffer(replay_buffer_size)
+        model_optim = ModelOptimizer(model, model_replay_buffer, lr=args.model_lr)
+
+        sac = SoftActorCritic(policy=policy_net,
+                              state_dim=state_dim,
+                              action_dim=action_dim,
+                              replay_buffer=replay_buffer,
+                              policy_lr=args.policy_lr,
+                              value_lr=args.value_lr,
+                              soft_q_lr=args.soft_q_lr)
+
+        planner = PathIntegral(model, policy_net, samples=args.trajectory_samples, t_H=args.horizon, lam=args.lam)
+
+        max_frames  = args.max_frames
+        max_steps   = args.max_steps
+        frame_skip = args.frame_skip
+
+        frame_idx   = 0
+        rewards     = []
+        batch_size  = 128
+
+        ep_num = 0
+
+        rate=rospy.Rate(10)
+
+        while frame_idx < max_frames:
+            state = env.reset()
+            planner.reset()
+
+            action = planner(state)
+
+            episode_reward = 0
+            for step in range(max_steps):
+                next_state, reward, self.done = env.step(action.copy())
+
+
+                next_action = planner(next_state)
+
+                replay_buffer.push(state, action, reward, next_state, self.done)
+                model_replay_buffer.push(state, action, reward, next_state, next_action, self.done)
+
+                if len(replay_buffer) > batch_size:
+                    sac.soft_q_update(batch_size)
+                    model_optim.update_model(batch_size, mini_iter=args.model_iter)
+
+                state = next_state
+                action = next_action
+                episode_reward += reward
+                frame_idx += 1
+
+                # if args.render:
+                #     env.render("human")
+
+                if frame_idx % int(max_frames/10) == 0:
+                    print(
+                        'frame : {}/{}, \t last rew : {}, \t rew loss : {}'.format(
+                            frame_idx, max_frames, rewards[-1][1], model_optim.log['rew_loss'][-1]
+                        )
+                    )
+
+                    pickle.dump(rewards, open(path + 'reward_data' + '.pkl', 'wb'))
+                    torch.save(policy_net.state_dict(), path + 'policy_' + str(frame_idx) + '.pt')
+
+                if self.done:
+                    print('done loop')
+                    break
+                else:
+                    rate.sleep()
+            #if len(replay_buffer) > batch_size:
+            #    for k in range(200):
+            #        sac.soft_q_update(batch_size)
+            #        model_optim.update_model(batch_size, mini_iter=1)#args.model_iter)
+            if len(replay_buffer) > batch_size:
+                print('ep rew', ep_num, episode_reward, model_optim.log['rew_loss'][-1], model_optim.log['loss'][-1])
+                print('ssac loss', sac.log['value_loss'][-1], sac.log['policy_loss'][-1], sac.log['q_value_loss'][-1])
+            rewards.append([frame_idx, episode_reward])
+            ep_num += 1
+        print('saving final data set')
+        pickle.dump(rewards, open(path + 'reward_data'+ '.pkl', 'wb'))
+        torch.save(policy_net.state_dict(), path + 'policy_' + 'final' + '.pt')
+
 
 if __name__ == '__main__':
-
     rospy.init_node('h_sac')
-    env = sawer_env()
-    manual_done = rospy.Service('/puck/done', Empty, doneCallback)
-
-    env_name = 'sawyer'
-    now = datetime.now()
-    date_str = now.strftime("%Y-%m-%d_%H-%M-%S/")
-
-    path = './data/' + env_name +  '/' + 'h_sac/' + date_str
-    if os.path.exists(path) is False:
-        os.makedirs(path)
-
-
-    action_dim = 2 # env.action_space.shape[0]
-    state_dim  = 4 # env.observation_space.shape[0]
-    hidden_dim = 128
-
-    policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim)
-
-    model = Model(state_dim, action_dim, def_layers=[200])
-
-    replay_buffer_size = 1000000
-    replay_buffer = ReplayBuffer(replay_buffer_size)
-
-    model_replay_buffer = SARSAReplayBuffer(replay_buffer_size)
-    model_optim = ModelOptimizer(model, model_replay_buffer, lr=args.model_lr)
-
-    sac = SoftActorCritic(policy=policy_net,
-                          state_dim=state_dim,
-                          action_dim=action_dim,
-                          replay_buffer=replay_buffer,
-                          policy_lr=args.policy_lr,
-                          value_lr=args.value_lr,
-                          soft_q_lr=args.soft_q_lr)
-
-    planner = PathIntegral(model, policy_net, samples=args.trajectory_samples, t_H=args.horizon, lam=args.lam)
-
-    max_frames  = args.max_frames
-    max_steps   = args.max_steps
-    frame_skip = args.frame_skip
-
-    frame_idx   = 0
-    rewards     = []
-    batch_size  = 128
-
-    ep_num = 0
-    while frame_idx < max_frames:
-        state = env.reset()
-        planner.reset()
-
-        action = planner(state)
-
-        episode_reward = 0
-        for step in range(max_steps):
-            for _ in range(frame_skip):
-                next_state, reward, done = env.step(action.copy())
-
-
-            next_action = planner(next_state)
-
-            replay_buffer.push(state, action, reward, next_state, done)
-            model_replay_buffer.push(state, action, reward, next_state, next_action, done)
-
-            if len(replay_buffer) > batch_size:
-                sac.soft_q_update(batch_size)
-                model_optim.update_model(batch_size, mini_iter=args.model_iter)
-
-            state = next_state
-            action = next_action
-            episode_reward += reward
-            frame_idx += 1
-
-            # if args.render:
-            #     env.render("human")
-
-            if frame_idx % int(max_frames/10) == 0:
-                print(
-                    'frame : {}/{}, \t last rew : {}, \t rew loss : {}'.format(
-                        frame_idx, max_frames, rewards[-1][1], model_optim.log['rew_loss'][-1]
-                    )
-                )
-
-                pickle.dump(rewards, open(path + 'reward_data' + '.pkl', 'wb'))
-                torch.save(policy_net.state_dict(), path + 'policy_' + str(frame_idx) + '.pt')
-
-            if args.done_util:
-                if done:
-                    break
-        #if len(replay_buffer) > batch_size:
-        #    for k in range(200):
-        #        sac.soft_q_update(batch_size)
-        #        model_optim.update_model(batch_size, mini_iter=1)#args.model_iter)
-        if len(replay_buffer) > batch_size:
-            print('ep rew', ep_num, episode_reward, model_optim.log['rew_loss'][-1], model_optim.log['loss'][-1])
-            print('ssac loss', sac.log['value_loss'][-1], sac.log['policy_loss'][-1], sac.log['q_value_loss'][-1])
-        rewards.append([frame_idx, episode_reward])
-        ep_num += 1
-    print('saving final data set')
-    pickle.dump(rewards, open(path + 'reward_data'+ '.pkl', 'wb'))
-    torch.save(policy_net.state_dict(), path + 'policy_' + 'final' + '.pt')
+    test = h_sac()
+    test.main()
