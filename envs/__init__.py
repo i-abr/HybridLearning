e427a1b111f45ed59ad52ddb282560e7d6cc4615
Ian Abraham <iabr4073@gmail.com>
2020-01-27 22:53:39 -0600

    added rex example

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,9 +1,9 @@
 from pybullet_envs import gym_pendulum_envs, gym_manipulator_envs, gym_locomotion_envs
 from gym.envs import classic_control, box2d
 from pybullet_envs.bullet.racecarGymEnv import RacecarGymEnv
-
 from pybullet_envs.bullet.minitaur_gym_env import MinitaurBulletEnv
 
+from rex_gym.envs.gym.galloping_env import RexReactiveEnv
 
 env_list = {
     'InvertedPendulumSwingupBulletEnv' : gym_pendulum_envs.InvertedPendulumSwingupBulletEnv,
@@ -13,7 +13,7 @@ env_list = {
     'ReacherBulletEnv' : gym_manipulator_envs.ReacherBulletEnv,
     'PendulumEnv' : classic_control.PendulumEnv,
     'LunarLanderContinuousEnv' : box2d.LunarLanderContinuous,
-    'KukaEnv' : MinitaurBulletEnv
+    'RexEnv' : RexReactiveEnv
 }
 
 def getlist():
