5ed73a3caea06ba4d76cd60d79823f0d7d965d58
Ian Abraham <iabr4073@gmail.com>
2020-07-17 12:48:30 -0500

    deletd the obsolete examples folder

--- model/model.py
+++ model/model.py
@@ -4,12 +4,6 @@ import torch.nn.functional as F
 from torch.distributions import Normal
 
 class Model(nn.Module):
-    """
-    Class creates a model for the dynamics system
-    a model of the reward function is also created
-    TODO: make this a MDN so that the model can generalize better
-    ### Words of matt; machine teaching
-    """
     def __init__(self, num_states, num_actions,
                  def_layers=[200, 200], std=0.):
 
@@ -40,13 +34,13 @@ class Model(nn.Module):
         dx, rew = forward(s, a)
         dx is the change in the state
         """
-        x   = torch.cat([s, a], axis=1)
+        _in   = torch.cat([s, a], axis=1)
 
         x = self.mu(x)
         # in case I want to update the way the var looks like
         std = torch.clamp(self.log_std, -10., 2).exp().expand_as(x)
 
-        return x+s, std, self.reward_fun(torch.cat([s, a], axis=1))
+        return x+s, std, self.reward_fun(_in)
 
     def step(self, x, u):
         mean, std, rew = self.forward(x, u)
